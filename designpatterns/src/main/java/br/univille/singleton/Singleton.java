cf6ca8bd842724c9c434d0fc2126d7a2cd6cdc26
Matheus Araújo <111009037+matheusaraujo002@users.noreply.github.com>
2025-08-19 00:58:36 +0000

    Singleton

---
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package br.univille.singleton;
+
+public class Singleton {
+    // Variável global
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
