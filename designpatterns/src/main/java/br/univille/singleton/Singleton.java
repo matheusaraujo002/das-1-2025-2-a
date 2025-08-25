e87ee77833b3fc818909a4f59e26fd686eb6bcf7
Matheus Araújo <111009037+matheusaraujo002@users.noreply.github.com>
2025-08-25 23:51:47 +0000

    update

--- designpatterns/src/main/java/br/univille/singleton/Singleton.java
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -3,13 +3,24 @@ package br.univille.singleton;
 public class Singleton {
     // Variável global
     private static Singleton instance;
+    private String segredo;
+
+    public String getSegredo() {
+        return segredo;
+    }
+
+    public void setSegredo(String segredo) {
+        this.segredo = segredo;
+    }
 
     private Singleton() {
-
+        System.out.println("Construtor do Singleton");
     }
 
     public static Singleton getInstance(){
+        System.out.println("getInstance");
         if(instance == null){
+            System.out.println("New Singleton");
             instance = new Singleton();
         }
         return instance;
